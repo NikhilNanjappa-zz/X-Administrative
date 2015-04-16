df1c9948d3072218e4564d6895087eee0dd9f3f6
Nikhil Nanjappa <nikhilnanjappa@Nikhils-MacBook-Pro.local>
2015-04-16 19:21:35 +0530

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xAdministrativeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xAdministrativeApp
+ */
+angular.module('xAdministrativeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xAdministrativeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xAdministrativeApp
+ */
+angular.module('xAdministrativeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
